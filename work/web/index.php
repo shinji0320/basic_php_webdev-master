d4501a35502d3faced648afe0c00bf229f3d499a
shinji0320 <shinji.blue.one@gmail.com>
2021-05-02 08:34:14 +0900

    空白, 改行を反映させる

--- work/web/index.php
+++ work/web/index.php
@@ -5,8 +5,7 @@ include('../app/_parts/_header.php');
 ?>
 
 <form action="result.php" method="get">
-  <input type="text" name="message">
-  <input type="text" name="username">
+  <textarea name="message"></textarea>
   <button>Send</button>
 </form>
 
