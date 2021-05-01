86ab89553ffdc9068a4c655639dd114f276d8aea
shinji0320 <shinji.blue.one@gmail.com>
2021-05-01 17:39:12 +0900

    [add]from

--- work/web/index.php
+++ work/web/index.php
@@ -1,31 +1,15 @@
 <?php
 
 require('../app/functions.php');
-
-$names = [
-  // 'Taro',
-  // 'Jiro',
-  // 'Saburo',
-];
-
+include('../app/_parts/_header.php');
 ?>
 
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="utf-8">
-  <title>PHP Practice</title>
-</head>
-<body>
-  <ul>
-    <?php if (empty($name)){ ?>
-      <li>Nobody!</li>
-    <?php }else{ ?>
-    <?php foreach($names as $name) { ?>
-      <li><?= h($name); ?></li>
-    <?php }?>
-    <?php }?>
-  </ul>
-  
-</body>
-</html>
+<form action="result.php" method="get">
+  <input type="text" name="message">
+  <input type="text" name="username">
+  <button>Send</button>
+</form>
+
+
+<?php
+include('../app/_parts/_footer.php');
